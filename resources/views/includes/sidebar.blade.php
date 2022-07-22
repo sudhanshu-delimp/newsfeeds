0eeb56209a1b9db574eb83bde347bbde9ed77ed9
Sudhanshu Bisht <sudhanshu@delimp.com>
2022-07-22 15:58:35 +0530

    d

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -13,7 +13,7 @@
           <img src="{{ asset('/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Admin</a>
+          <a href="#" class="d-block">{{ session()->get('account')->name }}</a>
         </div>
       </div>
 
