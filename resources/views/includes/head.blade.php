bcb0780ff2cd89a5ffa7967db3f06565183dcbd2
Sudhanshu Bisht <sudhanshu@delimp.com>
2022-07-22 13:14:32 +0530

    ok done

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -10,4 +10,5 @@
 <link rel="stylesheet" href="{{ asset('/plugins/fontawesome-free/css/all.min.css') }}">
 <!-- Theme style -->
 <link rel="stylesheet" href="{{ asset('/dist/css/adminlte.min.css') }}">
+<link rel="stylesheet" href="{{ asset('/dist/css/custom.css') }}">
 @stack('child-style')
